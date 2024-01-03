0417ee729eb4d1230a883c0f22013836c1dcaa3f
Maurício Szabo <mauricio.szabo@gmail.com>
2024-01-03 00:53:35 -0300

    Added more commands

--- src/chlorine2/core.cljs
+++ src/chlorine2/core.cljs
@@ -1,7 +1,9 @@
 (ns chlorine2.core
   (:require [chlorine.ui.atom :as atom]
-            [chlorine2.connections :refer [connect-nrepl!]]))
+            [chlorine2.connections :refer [connect-nrepl!]]
+            [chlorine.ui.inline-results :as inline]))
 
 (def commands
   (fn []
-    (clj->js {:connect #(connect-nrepl!)})))
+   (clj->js {:connect #(connect-nrepl!)
+             :clear-inline-results #(inline/clear-results! (atom/current-editor))})))
