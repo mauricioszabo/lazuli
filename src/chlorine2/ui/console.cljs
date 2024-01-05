dfdb815acbd55bdaa4af8806a9d6a13de6785bba
Maurício Szabo <mauricio.szabo@gmail.com>
2024-01-04 23:38:15 -0300

    Fixed native keybindings on console

--- src/chlorine2/ui/console.cljs
+++ src/chlorine2/ui/console.cljs
@@ -17,30 +17,35 @@
 (def ^:private Console (first console-pair))
 (def ^:private console (second console-pair))
 
-(defn open-console [state split destroy-fn]
-  (let [active (. js/document -activeElement)]
-    (aset console "destroy" destroy-fn)
-    (.. js/atom
-        -workspace
-        (open "atom://chlorine2-terminal" #js {:split split
-                                               :searchAllPanes true
-                                               :activatePane false
-                                               :state state
-                                               :activateItem false})
-        (then #(.focus active)))))
+(defonce div (doto (js/document.createElement "div")
+                   (aset "tabIndex" 1)
+                   (.. -classList (add "native-key-bindings"))))
+
+(defn open-console [split destroy-fn]
+  (let [active (. js/document -activeElement)
+        _ (aset console "destroy" destroy-fn)
+        p (.. js/atom
+              -workspace
+              (open "atom://chlorine2-terminal" #js {:split split
+                                                     :searchAllPanes true
+                                                     :activatePane false
+                                                     :activateItem false}))]
+    (.then p #(.focus active))
+    (.then p #(-> div .-childNodes first))))
 
 (defn register-console! [^js subs]
-  (let [scrolled? (atom true)
-        con (with-meta console/console-view
-              {:get-snapshot-before-update #(reset! scrolled? (console/all-scrolled?))
-               :component-did-update #(console/scroll-to-end! scrolled?)})]
+  (let [scrolled? (atom true)]
+        ; con (with-meta console/console-view
+        ;       {:get-snapshot-before-update #(reset! scrolled? (console/all-scrolled?))
+        ;        :component-did-update #(console/scroll-to-end! scrolled?)})]
     (.add subs
           (.. js/atom -workspace
-              (addOpener (fn [uri options]
+              (addOpener (fn [uri]
                            (when (= uri "atom://chlorine2-terminal")
-                             (rdom/render [con (.-state options) "native-key-bindings"]
-                                          console/div)
+                             (prn :WILl-OPEN)
+                             (aset div "innerHTML" "")
+                             (.appendChild div (console/view))
                              console)))))
-    (.add subs (.. js/atom -views (addViewProvider Console (constantly console/div))))))
+    (.add subs (.. js/atom -views (addViewProvider Console (constantly div))))))
 
-(defonce registered (register-console! @aux/subscriptions))
+(def registered (register-console! @aux/subscriptions))
