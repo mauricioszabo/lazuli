55767c4a901cc3ed50feb1b9633450a159ae10d9
Maurício Szabo <mauricio.szabo@gmail.com>
2024-01-13 16:34:29 -0300

    Autocomplete in Chlorine

--- src/chlorine/providers_consumers/autocomplete.cljs
+++ src/chlorine/providers_consumers/autocomplete.cljs
@@ -7,23 +7,34 @@
             [repl-tooling.eval :as eval]
             [repl-tooling.editor-integration.commands :as cmds]))
 
+(def tango-complete (atom nil))
+
 (def clj-var-regex #"[a-zA-Z0-9\-.$!?\/><*=\?_:]+")
 
 (defn- min-word-size []
   (.. js/atom -config (get "autocomplete-plus.minimumWordLength")))
 
-(defn- treat-result [prefix {:keys [candidate type]}]
-  {:text candidate
-   :type type
-   :replacementPrefix prefix})
+(defn- treat-result [re-prefix {:keys [completion/type text/contents]}]
+  (let [prefix (re-find re-prefix contents)]
+    {:text contents
+     :type type
+     :replacementPrefix prefix}))
 
-(defn suggestions [{:keys [^js editor]}]
-  (let [prefix (.. editor (getWordUnderCursor #js {:wordRegex clj-var-regex}))]
-    (when (-> prefix count (>= (min-word-size)))
-      (when-let [complete (some-> @state :tooling-state deref :editor/features :autocomplete)]
-        (.. (complete)
-            (then #(map (partial treat-result prefix) %))
-            (then clj->js))))))
+(def ^:private re-char-escapes
+  (->> "\\.*+|?()[]{}$^"
+       set
+       (map (juxt identity #(str "\\" %)))
+       (into {})))
+
+(defn- re-escape [prefix]
+  (str/escape (str prefix) re-char-escapes))
+
+(defn suggestions [{:keys [^js editor prefix] :as s}]
+  (p/let [completions (@tango-complete)
+          re-prefix (re-pattern (str ".*" (re-escape prefix)))]
+    (->> completions
+         (map (partial treat-result re-prefix))
+         clj->js)))
 
 (defn- meta-for-var [var]
   (p/let [state (:tooling-state @state)
