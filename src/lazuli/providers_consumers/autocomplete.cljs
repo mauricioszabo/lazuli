25ebf4afbeb63f4fa902d2b1591b2417e3557a4c
Maurício Szabo <mauricio.szabo@gmail.com>
2024-10-25 23:39:30 -0300

    Autocomplete code fix

--- src/lazuli/providers_consumers/autocomplete.cljs
+++ src/lazuli/providers_consumers/autocomplete.cljs
@@ -5,16 +5,23 @@
             [saphire.code-treatment :as treat]
             [orbit.evaluation :as eval]
             [saphire.complete :as complete]
+            [lazuli.state :as state]
             ["atom" :refer [Range]]))
 
-(defonce state (atom nil))
+#_
+(.. js/atom -ui -fuzzyMatcher (setCandidates #js ["foo" "foa"]) (match "fo"))
 
+; (defonce state (atom nil))
+
+#_
 (defn- min-word-size []
   (.. js/atom -config (get "autocomplete-plus.minimumWordLength")))
 
+#_
 (defn- re-escape [string]
   (str/replace string #"[\|\\\{\}\(\)\[\]\^\$\+\*\?\.\-\/]" "\\$&"))
 
+#_
 (defn- get-prefix [^js editor candidate]
   (let [non-word-chars (distinct (re-seq #"[^\w]" candidate))
         reg (re-pattern (str "[\\w" (re-escape (str/join "" non-word-chars)) "]+"))
@@ -24,26 +31,54 @@
                      (.getBufferPosition cursor))]
     (.getTextInRange editor word-range)))
 
-(defn- treat-result [editor [kind suggestion]]
-  (let [icon-name (case kind
-                    "local_var" "location status-modified"
-                    "pub_method" "organization status-renamed"
-                    "priv_method" "lock status-removed"
-                    "prot_method" "person status-modified"
-                    "instance_var" "mention status-renamed"
-                    "class_var" "mention status-renamed"
-                    "constant" "package status-renamed"
-                    "symbol" "tag-add status-renamed"
-                    "question")]
-    {:text suggestion
-     :iconHTML (str "<i class='icon-" icon-name "'></i>")
-     :replacementPrefix (get-prefix editor suggestion)}))
+(defn- treat-result [editor prefix {:keys [text/contents completion/type]}]
+  (prn :WAT?)
+  (let [[icon-name multiplier] (case type
+                                 :local ["location status-modified" 1]
+                                 :method/public ["organization status-renamed" 0.9]
+                                 :method/protected ["person status-modified" 0.8]
+                                 :method/private ["lock status-removed" 0.5]
+                                 :property ["mention status-renamed" 0.9]
+                                 :constant ["package status-renamed" 0.6]
+                                 :keyword ["tag-add status-renamed" 0.2]
+                                 ["question" 0.1])
+        filter (str/replace prefix #"^:" "")
+        match (.. js/atom -ui -fuzzyMatcher
+                  (match (str/replace contents #"^:" "") filter #js {:recordMatchIndexes true}))]
 
-(defn suggestions [{:keys [editor activatedManually]}]
-  (p/let [res (complete/suggestions @state)]
-    (->> res
-         (map #(treat-result editor %))
-         clj->js)))
+    ; (prn :C contents)
+    ; (prn :SUG prefix contents (.. js/atom -ui -fuzzyMatcher (score contents filter)))
+    (when-let [score (some-> match .-score)]
+      #js {:text contents
+           :score (* multiplier score)
+           :characterMatchIndices (.-matchIndexes match)
+           :iconHTML (str "<i class='icon-" icon-name "'></i>")
+           :replacementPrefix prefix})))
+
+#_
+(try
+  (->> completions
+               :completions/all
+               ; (#(doto % (prn :FIRST)))
+               (map #(treat-result editor (:completions/prefix completions) %))
+               (#(doto % (prn :SECOND)))
+               (sort-by #(- (.-score ^js %)))
+               (#(doto % (prn :THIRD)))
+               into-array)
+  (catch :default e
+    (prn :E e)))
+
+(defn suggestions [{:keys [^js editor activatedManually]}]
+  (p/let [state (-> editor .getGrammar .-name str/lower-case keyword state/get-state)
+          eql (-> @state :editor/features :eql)
+          completions (eql [{:editor/contents [:completions/all :completions/prefix]}])
+          completions (:editor/contents completions)]
+    (->> completions
+         :completions/all
+         (map #(treat-result editor (:completions/prefix completions) %))
+         (filter identity)
+         (sort-by #(- (.-score ^js %)))
+         into-array)))
 
 (defn- detailed-suggestion [suggestion])
 
@@ -57,7 +92,7 @@
 
          :suggestionPriority 200
 
-         :filterSuggestions true
+         :filterSuggestions false
 
          :getSuggestions (fn [data]
                            (-> data js->clj walk/keywordize-keys suggestions))
